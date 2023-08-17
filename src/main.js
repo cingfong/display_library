7ad1c8dd6ffd96d012894a4ac7cc145c30cd781d
cingfong <44254681+cingfong@users.noreply.github.com>
2023-08-17 11:04:03 +0800

    feat: silk-smooth-scroll setting element

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import smoothScroll from "vue-simple-smooth-scroll";
+import { inject } from "@vercel/analytics";
+
+inject();
 Vue.use(smoothScroll);
 
 Vue.config.productionTip = false;
--- src/views/silk-smooth-scroller.vue
+++ src/views/silk-smooth-scroller.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
     <header><h1>silk-smooth-scroll</h1></header>
+    <div class="silk-setting">
+      <p style="justify-content: center; font-size: 20px">Options</p>
+      <div class="setting-item-wrap">
+        <p>speed</p>
+        <input type="number" v-model="setting.speed" />
+      </div>
+      <div class="setting-item-wrap">
+        <p>transform</p>
+        <select v-model="setting.transform">
+          <option v-for="val in transformList" :key="val">{{ val }}</option>
+        </select>
+      </div>
+      <div class="setting-item-wrap justify-end">
+        <p>align</p>
+        <input type="checkbox" style="width: 20px" v-model="setting.align" />
+      </div>
+      <div class="setting-item-wrap">
+        <p>alignHeight</p>
+        <input type="text" v-model="setting.alignHeight" />
+      </div>
+      <div class="setting-item-wrap justify-end">
+        <p>direction</p>
+        <input
+          type="checkbox"
+          style="width: 20px"
+          v-model="setting.direction"
+        />
+      </div>
+      <div
+        class="setting-item-wrap"
+        v-for="(titleItem, index) in titleList"
+        :key="`titleItem-${index}`"
+      >
+        <p>title{{ index + 1 }}</p>
+        <input type="text" style="width: 40px" v-model="titleItem.number" />
+        <input type="text" style="width: 100px" v-model="titleItem.text" />
+      </div>
+      <div class="run" style="margin-top: 5px">
+        <div class="button" @click="run">run</div>
+      </div>
+    </div>
     <div id="silk-smooth">
       <div
         v-for="(item, index) in silkList"
@@ -16,34 +57,49 @@
 </template>
 <script>
 import silkSmooth from "silk-smooth-scroll";
-
 export default {
   data() {
     return {
-      silkList: [],
-    };
-  },
-  watch: {
-    silkList: function () {
-      const _title = [
-        {},
+      silkList: ["A", "B", "C", "D", "E"],
+      setting: {
+        speed: 0,
+        transform: "linear",
+        align: true,
+        alignHeight: 300,
+        direction: false,
+      },
+      titleList: [
+        { number: "", text: "" },
         { number: 1.1, text: "title first" },
         { number: 1.2, text: "title second" },
         { number: 1.3, text: "title third" },
         { number: 1.4, text: "title fourth" },
-      ];
-      setTimeout(() => {
-        silkSmooth.init({
-          titleClassName: "silk-smooth-title",
-          titleList: _title,
-        });
-      }, 0);
-    },
+      ],
+      transformList: ["ease", "ease-in", "ease-out", "ease-in-out", "linear"],
+    };
+  },
+  beforeDestroy() {
+    silkSmooth.remove();
   },
   mounted() {
-    this.$nextTick(() => {
-      this.silkList = ["A", "B", "C", "D", "E"];
-    });
+    this.silkInit();
+  },
+  methods: {
+    silkInit() {
+      silkSmooth.init({
+        titleClassName: "silk-smooth-title",
+        speed: this.setting.speed,
+        transformFunction: this.setting.transform ?? null,
+        align: this.setting.align ?? null,
+        alignHeight: this.setting.alignHeight ?? null,
+        direction: this.setting.direction ?? null,
+        titleList: this.titleList,
+      });
+    },
+    run() {
+      silkSmooth.remove();
+      this.silkInit();
+    },
   },
 };
 </script>
@@ -88,6 +144,54 @@ h1 {
 }
 
 .container5 {
-  background: linear-gradient(#7b2328, #020122);
+  background: linear-gradient(#7b2328, #171568);
+}
+.silk-setting {
+  position: fixed;
+  width: 220px;
+  height: 380px;
+  background-color: white;
+  right: 20px;
+  bottom: 20px;
+  z-index: 999;
+  padding: 5px;
+}
+.setting-item-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.silk-setting p {
+  display: flex;
+  font-size: 18px;
+  justify-content: space-between;
+  margin: 5px 0px;
+}
+.silk-setting input {
+  width: 100px;
+  height: 20px;
+  margin: 0px 10px;
+}
+.silk-setting select {
+  width: 100px;
+  height: 20px;
+  margin: 0px 10px;
+}
+.run {
+  display: flex;
+  justify-content: center;
+}
+.button {
+  display: inline-block;
+  width: 30px;
+  text-align: center;
+  background-color: red;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 18px;
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+  cursor: pointer;
 }
 </style>
